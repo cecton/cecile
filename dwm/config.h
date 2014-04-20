078cadaab3312b26856d3c336fe2758aa58dd38f
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-04-20 16:03:14 +0200

    Fixed: bad bindings in dwm
    ...forgot to change it when I set monocle to the default layout

--- dwm/config.h
+++ dwm/config.h
@@ -92,9 +92,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /*ntile*/
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, /*float*/
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, /*monocle*/
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} }, /*ntile*/
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} }, /*float*/
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[0]} }, /*monocle*/
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
