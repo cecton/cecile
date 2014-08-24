0c73255063b80b80a0003e2ff3675fa9fa7396ce
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-08-24 21:44:40 +0200

    Updated: dwm adapted to Thinkpad L440

--- dwm/config.h
+++ dwm/config.h
@@ -12,8 +12,8 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = False;    /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
-static const unsigned int forcewidth  = 1366;
-static const unsigned int forceheight = 768;
+static const unsigned int forcewidth  = 1600;
+static const unsigned int forceheight = 900;
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9",
@@ -39,7 +39,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact      = 0.80; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
