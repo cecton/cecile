87b82b48040949ee8dd1eb2281037df9dbab697f
Cecile Tonglet <cecile.tonglet@cecton.com>
2025-10-10 14:25:47 +0200

    cargo-temp: base nix environment

---
+++ .cache/cargo-temp/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.fenix.url = "github:nix-community/fenix";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, fenix, ... }:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    toolchain = fenix.packages.${pkgs.system}.fromToolchainFile {
+      file = ./rust-toolchain.toml;
+      sha256 = "sha256-SJwZ8g0zF2WrKDVmHrVG3pD2RGoQeo24MEXnNx5FyuI=";
+    };
+  in {
+    devShells.${pkgs.system}.default = pkgs.mkShell {
+      nativeBuildInputs = [
+        toolchain
+      ];
+    };
+  };
+}
