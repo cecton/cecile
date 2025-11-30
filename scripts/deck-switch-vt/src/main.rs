29d1599b932ebc700d694357fc6039c07a194548
Cecile Tonglet <cecile.tonglet@cecton.com>
2025-11-30 12:09:25 +0100

    deck-switch-vt: clear_env() before running commands for security

--- scripts/deck-switch-vt/src/main.rs
+++ scripts/deck-switch-vt/src/main.rs
@@ -4,11 +4,11 @@ const CHVT: &str = "/usr/bin/chvt";
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let fgconsole = Command::new(FGCONSOLE).output()?;
+    let fgconsole = Command::new(FGCONSOLE).env_clear().output()?;
     let vt = match fgconsole.stdout.as_slice().trim_ascii_end() {
         b"1" => "4",
         _ => "1",
     };
-    Command::new(CHVT).arg(vt).status()?;
+    Command::new(CHVT).env_clear().arg(vt).status()?;
     Ok(())
 }
