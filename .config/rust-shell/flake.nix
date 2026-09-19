97f9777952ac6abc8c07dcc82b974c83bd933abc
Cecile Tonglet <cecile.tonglet@cecton.com>
2026-09-19 15:04:00 +0200

    rust-shell: update

--- .config/rust-shell/flake.nix
+++ .config/rust-shell/flake.nix
@@ -10,34 +10,38 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        toolchain = pkgs.rust-bin.stable.latest.default.override {
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" "clippy" "rustfmt" "rust-analyzer" ];
-          targets = [ "wasm32-unknown-unknown" ];
+          targets = [ "wasm32-unknown-unknown" "x86_64-unknown-linux-musl" ];
         };
+
+        muslCc = pkgs.pkgsCross.musl64.stdenv.cc;
+
+        nativeDeps = with pkgs; [ pkg-config ] ++ [ muslCc ];
+        runtimeDeps = with pkgs; [
+          libx11
+          libxcursor
+          libxrandr
+          libxi
+          libxkbcommon
+          wayland
+          vulkan-loader
+          mesa
+          libGL
+          openssl
+        ];
+
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            toolchain
-            pkg-config
-            openssl
-            libx11
-            libxcursor
-            libxrandr
-            libxi
-            libxkbcommon
-            vulkan-loader
-            mesa
-            libGL
-          ];
+          buildInputs = [ rustToolchain ] ++ nativeDeps ++ runtimeDeps;
 
           shellHook = ''
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
-              pkgs.vulkan-loader
-              pkgs.libxkbcommon
-              pkgs.libx11
-              pkgs.mesa
-              pkgs.libGL
-            ]}:$LD_LIBRARY_PATH
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath runtimeDeps}:$LD_LIBRARY_PATH
+            export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER=${muslCc}/bin/x86_64-unknown-linux-musl-gcc
+            export CC_x86_64_unknown_linux_musl=${muslCc}/bin/x86_64-unknown-linux-musl-gcc
+            export CXX_x86_64_unknown_linux_musl=${muslCc}/bin/x86_64-unknown-linux-musl-g++
+            export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_RUSTFLAGS="-C target-feature=+crt-static"
           '';
         };
       }
